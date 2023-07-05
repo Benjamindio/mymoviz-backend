caea4e38aea08f82f649cede8b59cac79a691c51
Benjamin Diomat <benjamin.diomat@gmail.com>
2023-07-05 14:23:21 +0200

    commit backend

--- routes/movies.js
+++ routes/movies.js
@@ -10,18 +10,7 @@ router.get('/' , (req,res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(data => { 
-        const dataMovie =[]
-     for (let i =0 ; i < data.results.length;i++) {
-      const newMovie ={
-        title: data.results[i].title,
-        poster: `https://image.tmdb.org/t/p/w500${data.results[i].poster_path}`, 
-        voteAverage: data.results[i].vote_average, 
-        voteCount: data.results[i].vote_count, 
-        overview: data.results[i].overview, 
-      }
-      dataMovie.push(newMovie)
-     }
-        res.json({movies:dataMovie})
+        res.json({movies:data})
     })
 })
 
